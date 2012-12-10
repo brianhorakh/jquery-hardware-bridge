e0d196ba570ff688334be81c25b7da910321c56a
Brian Horakh <brianh@zoovy.com>
2012-12-10 11:57:42 -0800

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace ConsoleApplication1
+{
+    partial class DeviceServerMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.debugTxt = new System.Windows.Forms.TextBox();
+            this.portLbl = new System.Windows.Forms.Label();
+            this.portTxt = new System.Windows.Forms.TextBox();
+            this.debugLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // debugTxt
+            // 
+            this.debugTxt.CausesValidation = false;
+            this.debugTxt.Location = new System.Drawing.Point(23, 277);
+            this.debugTxt.Multiline = true;
+            this.debugTxt.Name = "debugTxt";
+            this.debugTxt.Size = new System.Drawing.Size(478, 186);
+            this.debugTxt.TabIndex = 0;
+            // 
+            // portLbl
+            // 
+            this.portLbl.AutoSize = true;
+            this.portLbl.Location = new System.Drawing.Point(33, 31);
+            this.portLbl.Name = "portLbl";
+            this.portLbl.Size = new System.Drawing.Size(58, 13);
+            this.portLbl.TabIndex = 1;
+            this.portLbl.Text = "HTTP Port";
+            this.portLbl.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // portTxt
+            // 
+            this.portTxt.Location = new System.Drawing.Point(97, 28);
+            this.portTxt.Name = "portTxt";
+            this.portTxt.Size = new System.Drawing.Size(80, 20);
+            this.portTxt.TabIndex = 2;
+            // 
+            // debugLbl
+            // 
+            this.debugLbl.AutoSize = true;
+            this.debugLbl.Location = new System.Drawing.Point(20, 261);
+            this.debugLbl.Name = "debugLbl";
+            this.debugLbl.Size = new System.Drawing.Size(80, 13);
+            this.debugLbl.TabIndex = 3;
+            this.debugLbl.Text = "Debug Console";
+            // 
+            // DeviceServerMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(526, 475);
+            this.Controls.Add(this.debugLbl);
+            this.Controls.Add(this.portTxt);
+            this.Controls.Add(this.portLbl);
+            this.Controls.Add(this.debugTxt);
+            this.Name = "DeviceServerMain";
+            this.Text = "DeviceServer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox debugTxt;
+        private System.Windows.Forms.Label portLbl;
+        private System.Windows.Forms.TextBox portTxt;
+        private System.Windows.Forms.Label debugLbl;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ConsoleApplication1
+{
+    public partial class DeviceServerMain : Form
+    {
+        public DeviceServerMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
+namespace MyTrayApp
+{
+    public class SysTrayApp : Form
+    {
+        [STAThread]
+        public static void Main()
+        {
+            Application.Run(new SysTrayApp());
+        }
+
+        private NotifyIcon  trayIcon;
+        private ContextMenu trayMenu;
+
+        public SysTrayApp()
+        {
+            // Create a simple tray menu with only one item.
+            trayMenu = new ContextMenu();
+            trayMenu.MenuItems.Add("Configure", OnConfigure);
+            trayMenu.MenuItems.Add("Exit", OnExit);
+
+            // Create a tray icon. In this example we use a
+            // standard system icon for simplicity, but you
+            // can of course use your own custom icon too.
+            trayIcon      = new NotifyIcon();
+            trayIcon.Text = "MyTrayApp";
+            trayIcon.Icon = new Icon(SystemIcons.Application, 40, 40);
+
+            // Add menu to tray icon and show it.
+            trayIcon.ContextMenu = trayMenu;
+            trayIcon.Visible     = true;
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            Visible       = false; // Hide form window.
+            ShowInTaskbar = false; // Remove from taskbar.
+
+            base.OnLoad(e);
+        }
+
+
+        private void OnConfigure(object sender, EventArgs e)
+        {
+            ConsoleApplication1.DeviceServerMain f = new ConsoleApplication1.DeviceServerMain();
+            f.ShowDialog();
+        }
+
+        private void OnExit(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        protected override void Dispose(bool isDisposing)
+        {
+            if (isDisposing)
+            {
+                // Release the icon resource.
+                trayIcon.Dispose();
+            }
+
+            base.Dispose(isDisposing);
+        }
+    }
+}
